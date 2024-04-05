40bb820b36b15c9a768950486136869e7eaeb27d
Oscar <oscarhv97@outlook.com>
2024-04-04 20:25:01 -0600

    done

--- AlertTest.java
+++ AlertTest.java
@@ -6,15 +6,18 @@ import org.testng.annotations.Test;
 public class AlertTest extends BaseTest {
     private final By JS_ALERT = By.xpath(String.format(PRECISE_TEXT_XPATH, "JavaScript Alerts"));
     private final By CLICK_FOR_JS_ALERT_BUTTON = By.xpath("//button[@onclick='jsAlert()']");
+    private final By SUCCESS_MSG_LOCATION = By.xpath(String.format(PRECISE_TEXT_XPATH, "You succesfully clicked on alert"));
+
+
+
 
     @Test
     public void alertTest() {
-        Assert.assertTrue(driver.findElement(By.xpath(JS_ALERT)).isDisplayed(), message("alert is not displayed"));
+        Assert.assertTrue(driver.findElement(By.xpath(JS_ALERT)).isDisplayed(), message: "alert is not displayed");
         driver.findElement(JS_ALERT).click();
-        Assert.assertTrue(!driver.findElements(By.xpath(CLICK_FOR_JS_ALERT_BUTTON)).isEmpty(), message("button alert is not displayed"));
-        driver.findElement(CLICK_FOR_JS_ALERT_BUTTON).click();
+        driver.switchTo().alert().accept()
+        Assert.assertTrue(driver.findElement(SUCCESS_MSG_LOCATION).isDisplayed(), message: "Success message is not displayed")
         
-
         // todo: accept alert
         // todo: assert success message
     }
